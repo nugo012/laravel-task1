8f9a76afb040527dd1c5949ff4b6481c10f299fa
student <student@BTU.local>
2023-10-07 14:23:48 +0400

    feat: add routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,22 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/name',function (){
+    return "nugo";
+});
+Route::get('/lastname',function (){
+    return "rostiashvili";
+});
+Route::get('/age',function (){
+    return 21;
+});
+Route::get('/hobby',function (){
+    return "Football";
+});
+Route::get('/city',function (){
+    return "Tbilisi";
+});
+Route::delete('user/{id}',function($id){
+    return response()->json(['msg'=>"delete request is successfull"]);
+});
